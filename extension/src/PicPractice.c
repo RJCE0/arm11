b4832240a90f4a8c283aef7677d88db462946e15
rj619 <reece.jackson19@imperial.ac.uk>
2020-06-15 04:30:52 +0100

    "Made a grey scale version"

--- extension/src/PicPractice.c
+++ extension/src/PicPractice.c
@@ -17,5 +17,26 @@ int main(void){
     }
     printf("loaded image with %d width, %d height and %d channels\n", width,height,channels);
 
+    size_t img_size = width * height * channels;
+    int grey_channels = channels == 4 ? 2 : 1;
+    size_t grey_img_size = width * height * grey_channels;
+
+    unsigned char *grey_img = malloc(grey_img_size);
+    if(grey_img == NULL) {
+        printf("Unable to allocate memory for the gray image.\n");
+        exit(EXIT_FAILURE); 
+    }
+
+    for(unsigned char *p = img, *pg = grey_img; p != img + img_size; 
+    p += channels, pg += grey_channels) {
+        *pg = (uint8_t)((*p + *(p + 1) + *(p + 2))/3.0);
+        if(channels == 4) {
+            *(pg + 1) = *(p + 3);
+        }
+    }
+
+    stbi_write_jpg("stb_image/EveryItemImageGrey.jpg", width, height, grey_channels, grey_img, 100);
+
     stbi_image_free(img);
+    free(grey_img);
 }
