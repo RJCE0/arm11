1f0323bca310cc9306e2695c52362310ba661ec9
rj619 <reece.jackson19@imperial.ac.uk>
2020-06-16 03:53:19 +0100

    "All images done"

--- extension/src/PicPractice.c
+++ extension/src/PicPractice.c
@@ -6,17 +6,33 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
 
-//Basics of how to load an image
-int main(void){
+stbi_uc *load(char const *fileName){
     int width, height, channels;
-    unsigned char *img = stbi_load("stb_image/EveryItemImage.jpg", 
+    unsigned char *img = stbi_load(fileName, 
     &width, &height, &channels, 0);
     if (img == NULL){
         perror("failed to load image");
         exit(EXIT_FAILURE);
     }
-    printf("loaded image with %d width, %d height and %d channels\n", width,height,channels);
+    printf("loaded image with %d width, %d height and %d channels\n", width, height, channels);
+    return img;
+}
 
+//Basics of how to load an image
+int main(void){ 
+    unsigned char *q1 = load("stb_image/Q1.jpg");
+    unsigned char *q1Ans = load("stb_image/Q1Ans.jpg");
+    unsigned char *q2 = load("stb_image/Q2.jpg");
+    unsigned char *q2Ans = load("stb_image/Q2Ans.jpg");
+
+    
+    stbi_image_free(q1);
+    stbi_image_free(q1Ans);
+    stbi_image_free(q2);
+    stbi_image_free(q2Ans);
+
+/* 
+    How to make an image grey scale
     size_t img_size = width * height * channels;
     int grey_channels = channels == 4 ? 2 : 1;
     size_t grey_img_size = width * height * grey_channels;
@@ -36,7 +52,7 @@ int main(void){
     }
 
     stbi_write_jpg("stb_image/EveryItemImageGrey.jpg", width, height, grey_channels, grey_img, 100);
-
-    stbi_image_free(img);
     free(grey_img);
+*/
+
 }
